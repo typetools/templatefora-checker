71082299b4ac2f3a64178475da8a0a6849a305dc
Michael Ernst <mernst@cs.washington.edu>
2019-05-25 15:37:48 -0700

    Reformat

--- src/main/java/org/checkerframework/checker/templatefora/qual/TemplateforaUnknown.java
+++ src/main/java/org/checkerframework/checker/templatefora/qual/TemplateforaUnknown.java
@@ -8,8 +8,8 @@ import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 /**
- * The default qualifier in the hierarchy. This is the top type in the lattice,
- * which represents that no information is available.
+ * The default qualifier in the hierarchy. This is the top type in the lattice, which represents
+ * that no information is available.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
--- src/test/java/tests/TemplateforaTest.java
+++ src/test/java/tests/TemplateforaTest.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.util.List;
-
 import org.checkerframework.checker.templatefora.TemplateforaChecker;
 import org.checkerframework.framework.test.CheckerFrameworkPerDirectoryTest;
 import org.junit.runners.Parameterized.Parameters;
@@ -8,9 +7,10 @@ import org.junit.runners.Parameterized.Parameters;
 /**
  * Test runner for tests of the TemplateForA Checker.
  *
- * <p>Tests appear as Java files in the {@code tests/templatefora} folder. To add a new test case, create
- * a Java file in that directory. The file contains "// ::" comments to indicate expected errors and
- * warnings; see https://github.com/typetools/checker-framework/blob/master/checker/tests/README .
+ * <p>Tests appear as Java files in the {@code tests/templatefora} folder. To add a new test case,
+ * create a Java file in that directory. The file contains "// ::" comments to indicate expected
+ * errors and warnings; see
+ * https://github.com/typetools/checker-framework/blob/master/checker/tests/README .
  */
 public class TemplateforaTest extends CheckerFrameworkPerDirectoryTest {
     public TemplateforaTest(List<File> testFiles) {
